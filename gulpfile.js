eadcd1b28aac18341218b8d97b65d1db6f20a1eb
Anil Maharjan <simpleanilcha@gmail.com>
2015-10-09 22:11:01 +0545

    Added gulp-sass for scss compiling.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,23 @@
-var gulp = require ('gulp');
+var gulp = require ('gulp'),
+	sass = require ('gulp-sass');
 
 
-gulp.task('default', function(){
+gulp.task('test', function(){
 	console.log('hello');
 });
+
+gulp.task('sass', function(){
+	gulp.src('components/scss/*.scss')
+	.pipe(sass({
+		outputStyle: 'expanded'
+	}).on('error', sass.logError))
+	.pipe(gulp.dest('././builds/development/css'))
+});
+
+
+// watching
+gulp.task('watch', function(){
+	gulp.watch('components/scss/*.scss', ['sass']);
+});
+
+gulp.task('default', ['watch']);
