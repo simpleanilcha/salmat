c477338fd519cec04aeec6d35701a2ed00b22306
Anil Maharjan <simpleanilcha@gmail.com>
2015-10-09 23:12:47 +0545

    Added bower, bootstrap, gulp-rename, gulp-minify-css, gulp-uglify.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require ('gulp'),
-	sass = require ('gulp-sass');
+	sass = require ('gulp-sass'),
+	uglify = require ('gulp-uglify'),
+	rename = require ('gulp-rename'),
+	minifyCss = require ('gulp-minify-css');
 
 
 gulp.task('test', function(){
@@ -14,6 +17,27 @@ gulp.task('sass', function(){
 	.pipe(gulp.dest('././builds/development/css'))
 });
 
+// uglify bootstrap.js to bootstrap.min.js
+gulp.task('uglifyPlugins', function(){
+	gulp.src('components/libs/bootstrap/dist/js/bootstrap.js')
+	.pipe(rename({
+		suffix: '.min',
+		extname: '.js'
+	}))
+	.pipe(uglify())
+	.pipe(gulp.dest('builds/development/js'))
+});
+
+// minify boostrap.css
+gulp.task('minifyPlugins', function(){
+	gulp.src('components/libs/bootstrap/dist/css/bootstrap.css')
+	.pipe(rename({
+		suffix: '.min',
+		extname: '.css'
+	}))
+	.pipe(minifyCss({compatibility: 'ie8'}))
+	.pipe(gulp.dest('builds/development/css'))
+})
 
 // watching
 gulp.task('watch', function(){
